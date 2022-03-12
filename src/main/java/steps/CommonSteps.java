f06252a54ada50cc85d7afb60933faf8972d180e
maciej.wawryczuk@ziflow.com <Maciekschybaty01>
2022-03-12 10:45:04 +0100

    1st line of test

---
+++ src/main/java/steps/CommonSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+public class CommonSteps {
+
+    @Given("Lista 10 paczkomatów jest widoczna")
+    public void inPostListIsVisible() {
+
+    }
+}
---
+++ src/test/resources/features/InPost.feature
@@ -0,0 +1,4 @@
+Feature: Paczkomaty
+
+  Scenario: Powinien zwrócić listę 10 paczkomatów w Warszawie
+    Given Lista 10 paczkomatów jest widoczna
